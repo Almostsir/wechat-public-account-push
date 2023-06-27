b2ec67b90a0640663e441b5a9167078137b81f2a
杨永康 <31638893+Almostsir@users.noreply.github.com>
2023-06-27 17:28:35 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,7 +6,7 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
-  USE_PASSAGE: 'push-plus',
+  USE_PASSAGE: 'server-chan',
   // 使用微信测试号：公众号APP_ID
   APP_ID: '',
 
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: 'Li',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'b37cc42faa98434ab4b948e98f7d1a36',
+      id: 'SCT214614TXlxLiIwDEY7s5jE7VdA2ghMH',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -57,7 +57,7 @@ const USER_CONFIG = {
     {
       name: '张起灵',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'b37cc42faa98434ab4b948e98f7d1a36',
+      id: 'SCT214614TXlxLiIwDEY7s5jE7VdA2ghMH',
     }
   ],
 
